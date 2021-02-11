908e2fdd5c8eb69bb26c615813e2840bcbfec238
simon <tran.simon2@gmail.com>
2021-02-11 15:44:16 -0500

    INF3995 #6 - updated dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,30 @@
 FROM python:3
 
-WORKDIR /usr/src/app
+RUN apt-get update && apt-get install -y \
+	usbutils
+
+
+WORKDIR /root/backend
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
-COPY src/ .
 
+
+COPY . .
+
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_APP=src/app.py
+ENV FLASK_ENV=development
 ENV PORT=5000
 
 EXPOSE 5000
 
-CMD ["flask", "run", "--host=0.0.0.0"]
+RUN mkdir  -p /etc/udev/rules.d
+RUN echo '# Crazyradio (normal operation) \
+          SUBSYSTEM=="usb", ATTRS{idVendor}=="1915", ATTRS{idProduct}=="7777", MODE="0664", GROUP="plugdev" \
+          # Bootloader \
+          SUBSYSTEM=="usb", ATTRS{idVendor}=="1915", ATTRS{idProduct}=="0101", MODE="0664", GROUP="plugdev" \
+          SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE="0664", GROUP="plugdev" ' > /etc/udev/rules.d/99-crazyradio.rules
+
+
+CMD ["flask", "run"]
--- src/app.py
+++ src/app.py
@@ -2,26 +2,17 @@ from flask import Flask
 from flask_cors import CORS
 from pyrebase import pyrebase
 from pyngrok import ngrok
-from Appchannel import AppchannelCommunicate 
+from Appchannel import AppchannelCommunicate
+import time
 import cflib
 from cflib.crazyflie import Crazyflie
 
 app = Flask(__name__)
 CORS(app)
 
-cflib.crtp.init_drivers(enable_debug_driver=False)
-print('Scanning interfaces for Crazyflies...')
-available = cflib.crtp.scan_interfaces()
-print('Crazyflies found:')
-if len(available) > 0:
-    comm = AppchannelCommunicate(available[0][0])
-else:
-    print('No Crazyflies found, cannot run example')
-
-
-
 activated = 0
 battery = 100
+comm = None
 
 public_url = ngrok.connect(5000).public_url
 
@@ -38,13 +29,39 @@ privatize = split_url[0] + "s:" + split_url[1]
 firebase = pyrebase.initialize_app(config).database()
 firebase.update({"url": privatize})
 
+@app.route('/')
+def main():
+    global battery
+    global activated
+
+    return 'Activated: {}, Battery: {}'.format(activated, battery)
+
+
+@app.route('/scan')
+def scan():
+    global comm
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+    print('Scanning interfaces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+    print('Crazyflies found:')
+    if len(available) > 0:
+        comm = AppchannelCommunicate(available[0][0])
+        return 'Found a Crazyflie'
+    else:
+        print('No Crazyflies found, waiting 3 seconds')
+        return 'No Crazyflies found', 500
+
 
 @app.route("/changeState")
 def changeState():
     global activated
-    activated = activated ^ 1
-    comm._sendPacket(b'l')
-    return {'result': activated}
+    try:
+        activated = activated ^ 1
+        comm._sendPacket(b'l')
+        return {'result': activated}
+    except:
+        print("Error")
+        return 'Error', 500
 
 
 @app.route("/getState")
@@ -56,8 +73,10 @@ def getState():
 @app.route("/getBatteryLevel")
 def getBatteryLevel():
     global battery
-    comm._sendPacket(b'b')
-    battery = "{:.2f}".format(comm._getBatteryLevel())
-    return {'result': battery}
-
-
+    try:
+        comm._sendPacket(b'b')
+        battery = "{:.2f}".format(comm._getBatteryLevel())
+        return {'result': battery}
+    except:
+        print("Error")
+        return 'Error', 500
