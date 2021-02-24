c295dbe4eae0b7be8bdb39d1ecb6c43ac8429838
simon <tran.simon2@gmail.com>
2021-02-23 19:06:46 -0500

    removed speed

--- src/DroneDTO.py
+++ src/DroneDTO.py
@@ -4,9 +4,7 @@ from Drone import Drone
 class DroneDTO:
     droneId = ''
     battery = -1
-    speed = -1
 
     def  __init__(self, drone: Drone ):
         self.droneId = drone.getId()
         self.battery = drone.getChannel().getBatteryLevel()
-        self.speed = drone.getChannel().getSpeed()
