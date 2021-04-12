b53bbb373aebc472d89df2384bb53e6c48779fa6
Laurent <laurent.bolduc@polymtl.ca>
2021-04-12 17:17:07 -0400

    Added tests for app

--- src/tests/test_App.py
+++ src/tests/test_App.py
@@ -4,6 +4,7 @@ import requests
 import os
 sys.path.append("..")
 from App import *
+import App
 
 
 def test_before_first_request_should_call_os_path_dirname(mocker):
@@ -19,11 +20,13 @@ def test_get_latest_data_should_return_the_data_for_the_specified_type():
 
 def test_reset_should_set_is_sim_to_false_if_http_parameter_is_false():
     expected_value = False
+    droneList.append(Dronesim(str(1), "test"))
     response = requests.get('http://0.0.0.0:5000/reset', params={'simulation': 'false'})
     assert expected_value == response.json()
 
 def test_reset_should_set_is_sim_to_true_if_http_parameter_is_true():
     expected_value = True
+    simDroneList.append(Dronesim(str(1), "test"))
     response = requests.get('http://0.0.0.0:5000/reset', params={'simulation': 'true'})
     assert expected_value == response.json()
 
@@ -32,13 +35,33 @@ def test_reset_should_set_is_sim_to_true_if_http_parameter_is_true():
 
 def test_flash_should_return_the_request_to_the_firmware_server():
     #Le server n'est pas actif, alors la reponse contiendra une erreur
-    expected_value = '<Response [500]>'
+    expected_value = 500
     response = requests.get('http://0.0.0.0:5000/flash', params={})
-    assert expected_value == str(response)
+    assert expected_value == response.status_code
 
-# def test_logs_should_call_send_file(mocker):
-#     expected_value = 0
-#     mocker.patch('flask.send_file', return_value=0)
-#     actual_value = logs()
-#     assert expected_value == actual_value
+def test_update_stats_should_return_a_json():
+    expected_value = []
+    requests.get('http://0.0.0.0:5000/reset', params={'simulation': 'false'})
+    response = requests.get('http://0.0.0.0:5000/updateStats')
+    assert expected_value == response.json()
+    requests.get('http://0.0.0.0:5000/reset', params={'simulation': 'true'})
+    response = requests.get('http://0.0.0.0:5000/updateStats')
+    assert expected_value == response.json()
+
+def test_live_check_returns_sim_string():
+    expected_value = {'simulation': True}
+    response = requests.get('http://0.0.0.0:5000/liveCheck')
+    assert expected_value == response.json()
+    requests.get('http://0.0.0.0:5000/reset', params={'simulation': 'false'})
+    expected_value = {'simulation': False}
+    response = requests.get('http://0.0.0.0:5000/liveCheck')
+    assert expected_value == response.json()
+
+
+def test_logs_should_call_send_file():
+    # ok
+    expected_value = 200
+    response = requests.get('http://0.0.0.0:5000/logs')
+
+    assert expected_value == response.status_code
  
