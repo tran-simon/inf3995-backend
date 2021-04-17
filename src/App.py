558135b7dcc34133ee7ae4b13b34ceb156945332
Matthew <matthew-justin.paoli@polymtl.ca>
2021-04-17 14:19:57 -0400

    Took out temporary sendPacket (point)

--- src/App.py
+++ src/App.py
@@ -73,7 +73,6 @@ def updateStats():
                 drone.getChannel().sendPacket(b'b')
                 drone.getChannel().sendPacket(b'v')
                 drone.getChannel().sendPacket(b's')
-                drone.getChannel().sendPacket(b'p')
                 drone.getChannel().sendPacket(b'm')
             except:
                 continue
